23d37407a9c31291d76b31fb8c5d8aa31d882483
Sean Malloy <sfmalloy@outlook.com>
2022-12-30 23:05:50 -0500

    Day 9

--- solutions/d09.py
+++ solutions/d09.py
@@ -1,17 +1,37 @@
 from io import TextIOWrapper
-from dataclasses import dataclass
+
+
+def expand(string: str):
+    print(string)
+    i = 0
+    size = 0
+    while i < len(string):
+        if string[i] != '(':
+            i += 1
+            size += 1
+        else:
+            end = string.index(')', i)
+            num, reps = map(int, string[i+1:end].split('x'))
+            i = end+1
+            size += expand(string[i:i+num]) * reps
+            i += num
+    return size
 
 
 def main(file: TextIOWrapper):
     string = file.read().strip()
+    size = 0
     i = 0
-    decomp = ''
     while i < len(string):
         if string[i] != '(':
-            decomp += string[i]
             i += 1
+            size += 1
         else:
             end = string.index(')', i)
+            num, reps = map(int, string[i+1:end].split('x'))
+            i = end+1
+            size += num * reps
+            i += num
 
 
-    return -1,-1
+    return size,expand(string)
